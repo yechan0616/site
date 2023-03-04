dccd15e8ee308ed511ef825f723b61bb96c791e2
바나나 <83855546+milk0616@users.noreply.github.com>
2023-03-04 10:51:08 +0900

    Add files via upload

---
+++ assets/js/main.fullPage.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('#fullpage').fullpage({
+		autoScrolling:true,
+		scrollHorizontally: true,
+        navigation:true,
+        navigationPosition:'right',
+        sectionsColor: ['#F0F8FF', '#F0F8FF', '#F0F8FF', '#F0F8FF']
+	});
+});
